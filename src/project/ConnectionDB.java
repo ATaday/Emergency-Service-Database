737032d02f4bf6ba39ea76fc31aa953e1740b1fa
Alper Taday <trshield@gmail.com>
2015-07-06 15:35:12 +0300

    Update ConnectionDB.java

--- src/project/ConnectionDB.java
+++ src/project/ConnectionDB.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionDB { // tamam
+public class ConnectionDB { 
 
 	static Connection connection = null;
 
